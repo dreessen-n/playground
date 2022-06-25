a0b09ca5c2823d221761be5d7ea5d82a5023a1f4
dreessen-n <dreessen.edu@gmail.com>
2022-06-24 22:20:31 -0600

    Initial comit

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/play')
+def play():
+    return render_template('index.html', num=3, color='aquamarine')
+
+# Add in an error message for page not found - 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return f'Sorry! No response. Try again.'
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(host='localhost', port=5001, debug=True)    # Run the app in debug mode.
